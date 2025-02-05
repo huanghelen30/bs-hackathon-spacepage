2de60cfee926eac1f8d0c70259eda7d0a38c9af2
Daria Sergeeva <sergeevadariavl@gmail.com>
2025-02-05 11:23:48 -0800

    Added Lost Files

--- scripts/picture.js
+++ scripts/picture.js
@@ -1,19 +1,41 @@
 
-// function getFact() {
-//   fetch()
-//     .then((response) => response.json())
-//     .then((data) => {
-//       document.getElementById("fact").innerText = data.text;
-//     });
-// }
+/* function getFact() {
+  fetch()
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("fact").innerText = data.text;
+    });
+}
 
-const imageApi = new ImageApi(API_KEY)
+const factsApi = new FactsApi(API_KEY)
 
 async function getFact() {
     try {
-      const image = await imageApi.getImage();
+      const image = await factsApi.getImage();
     } catch (error) {
       console.error(error);
   }
-};
+}; 
 
+/* Alternative Code */
+console.log("Hello team");
+
+function getImage() {
+  fetch(" ") 
+    .then((response) => response.json())
+    .then((data) => {
+        const imageElement = document.getElementById("image");  element
+
+        if (data.url.endsWith(".mp4")) { 
+          console.log("Skipping video file...");
+          getImage(); 
+        } else {
+          imageElement.src = data.url; 
+        }
+    })
+    .catch((error) => {
+      console.error("Error fetching image", error);
+      document.getElementById("image").src = "fallback-image.jpg"; 
+    });
+}
+/* That was my initial Java Script code I failed to commit - Daria */ 
