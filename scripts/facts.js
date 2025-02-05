41cab086fbf017e07162084f7f3d1e3fc922a091
Helen <huanghelen30@gmail.com>
2025-02-05 09:37:14 -0800

    add api for image

--- scripts/facts.js
+++ scripts/facts.js
@@ -1,12 +1,8 @@
 
 function getFact() {
-<<<<<<< HEAD
-  axios.get
-=======
   fetch()
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("fact").innerText = data.text;
     });
->>>>>>> 3ba5cedc794a5d9ea85f77528e011b3ebbfb290a
 }
